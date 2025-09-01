0a3a519220b09d04c69f0d9315db53d32634c285
RHRolun <rlundber@redhat.com>
2025-09-01 22:31:29 +0200

    small update

--- parts-pipeline/prod_train_save_pipeline.py
+++ parts-pipeline/prod_train_save_pipeline.py
@@ -116,6 +116,8 @@ def training_pipeline(hyperparameters: dict, model_name: str, version: str, clus
     
 
 if __name__ == '__main__':
+    COMPILE = True
+
     metadata = {
         "hyperparameters": {
             "epochs": 2
@@ -126,32 +128,35 @@ if __name__ == '__main__':
         "model_storage_pvc": "jukebox-model-pvc",
         "prod_flag": False
     }
-        
-    namespace_file_path =\
-        '/var/run/secrets/kubernetes.io/serviceaccount/namespace'
-    with open(namespace_file_path, 'r') as namespace_file:
-        namespace = namespace_file.read()
-
-    kubeflow_endpoint =\
-        f'https://ds-pipeline-dspa.{namespace}.svc:8443'
-
-    sa_token_file_path = '/var/run/secrets/kubernetes.io/serviceaccount/token'
-    with open(sa_token_file_path, 'r') as token_file:
-        bearer_token = token_file.read()
-
-    ssl_ca_cert =\
-        '/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt'
-
-    print(f'Connecting to Data Science Pipelines: {kubeflow_endpoint}')
-    client = kfp.Client(
-        host=kubeflow_endpoint,
-        existing_token=bearer_token,
-        ssl_ca_cert=ssl_ca_cert
-    )
-
-    client.create_run_from_pipeline_func(
-        training_pipeline,
-        arguments=metadata,
-        experiment_name="kfp-training-pipeline",
-        enable_caching=True
-    )
+    
+    if COMPILE:
+        kfp.compiler.Compiler().compile(training_pipeline, 'parts-pipeline.yaml', pipeline_parameters=metadata)
+    else:
+        namespace_file_path =\
+            '/var/run/secrets/kubernetes.io/serviceaccount/namespace'
+        with open(namespace_file_path, 'r') as namespace_file:
+            namespace = namespace_file.read()
+    
+        kubeflow_endpoint =\
+            f'https://ds-pipeline-dspa.{namespace}.svc:8443'
+    
+        sa_token_file_path = '/var/run/secrets/kubernetes.io/serviceaccount/token'
+        with open(sa_token_file_path, 'r') as token_file:
+            bearer_token = token_file.read()
+    
+        ssl_ca_cert =\
+            '/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt'
+    
+        print(f'Connecting to Data Science Pipelines: {kubeflow_endpoint}')
+        client = kfp.Client(
+            host=kubeflow_endpoint,
+            existing_token=bearer_token,
+            ssl_ca_cert=ssl_ca_cert
+        )
+    
+        client.create_run_from_pipeline_func(
+            training_pipeline,
+            arguments=metadata,
+            experiment_name="kfp-training-pipeline",
+            enable_caching=True
+        )
